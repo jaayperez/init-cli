8cc81dc5ce2bc8e3d90a8bc4052cb2db583b7e2f
Justin Perez <justin@startrev.com>
2020-11-29 00:52:04 -0800

    Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -66,29 +66,32 @@ function createProject(projectPath: string) {
 // list of file/folder that should not be copied
 const SKIP_FILES = ['node_modules', '.template.json'];
 function createDirectoryContents(templatePath: string, projectName: string) {
-    // read all files/folders (1 level) from template folder
-    const filesToCreate = fs.readdirSync(templatePath);
-    // loop each file/folder
-    filesToCreate.forEach(file => {
-        const origFilePath = path.join(templatePath, file);
+  // read all files/folders (1 level) from template folder
+  const filesToCreate = fs.readdirSync(templatePath);
+  // loop each file/folder
+  filesToCreate.forEach(file => {
+    const origFilePath = path.join(templatePath, file);
 
-        // get stats about the current file
-        const stats = fs.statSync(origFilePath);
+    // get stats about the current file
+    const stats = fs.statSync(origFilePath);
 
-        // skip files that should not be copied
-        if (SKIP_FILES.indexOf(file) > -1) return;
+    // skip files that should not be copied
+    if (SKIP_FILES.indexOf(file) > -1) return;
 
-        if (stats.isFile()) {
-            // read file content and transform it using template engine
-            let contents = fs.readFileSync(origFilePath, 'utf8');
-            // write file to destination folder
-            const writePath = path.join(CURR_DIR, projectName, file);
-            fs.writeFileSync(writePath, contents, 'utf8');
-        } else if (stats.isDirectory()) {
-            // create folder in destination folder
-            fs.mkdirSync(path.join(CURR_DIR, projectName, file));
-            // copy files/folder inside current folder recursively
-            createDirectoryContents(path.join(templatePath, file), path.join(projectName, file));
-        }
-    });
+    if (stats.isFile()) {
+      // read file content and transform it using template engine
+      let contents = fs.readFileSync(origFilePath, 'utf8');
+
+      contents = template.render(contents, {projectName});
+
+      // write file to destination folder
+      const writePath = path.join(CURR_DIR, projectName, file);
+      fs.writeFileSync(writePath, contents, 'utf8');
+    } else if (stats.isDirectory()) {
+      // create folder in destination folder
+      fs.mkdirSync(path.join(CURR_DIR, projectName, file));
+      // copy files/folder inside current folder recursively
+      createDirectoryContents(path.join(templatePath, file), path.join(projectName, file));
+    }
+  });
 }
