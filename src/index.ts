f9dca4f3380d3f00f325094bb3fee12ffcc52d51
Justin Perez <justin@startrev.com>
2020-11-28 22:19:31 -0800

    Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
-console.log('Hi there!')
+import * as inquirer from 'inquirer';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const CHOICES = fs.readdirSync(path.join(__dirname, 'templates'));
+
+const QUESTIONS = [
+{
+    name: 'template',
+    type: 'list',
+    message: 'What project template would you like to start with?',
+    choices: CHOICES
+},
+{
+    name: 'name',
+    type: 'input',
+    message: 'Project name:'
+}];
+
+inquirer.prompt(QUESTIONS)
+.then(answers => {
+    console.log(answers);
+});
